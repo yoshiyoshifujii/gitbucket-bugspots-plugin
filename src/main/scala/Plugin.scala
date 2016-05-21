9efb713d405e95c4b46f7c3e0129ea2d2a06ab14
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2016-05-21 16:33:14 +0900

    GitBucket 4.0.0 support.

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -1,8 +1,6 @@
-
-
 import gitbucket.core.plugin.PluginRegistry
 import gitbucket.core.service.SystemSettingsService.SystemSettings
-import gitbucket.core.util.Version
+import io.github.gitbucket.solidbase.model.Version
 import javax.servlet.ServletContext
 import me.huzi.gitbucket.bugspots.controller.BugSpotsController
 
@@ -14,8 +12,9 @@ class Plugin extends gitbucket.core.plugin.Plugin {
   override val description: String = "Bug Prediction at Google"
 
   override val versions: Seq[Version] = Seq(
-    Version(3, 12),
-    Version(1, 0))
+    new Version("4.0.0"),
+    new Version("3.12"),
+    new Version("1.0"))
 
   override val controllers = Seq(
     "/*" -> new BugSpotsController
@@ -26,31 +25,35 @@ class Plugin extends gitbucket.core.plugin.Plugin {
     val path = settings.baseUrl.getOrElse(context.getContextPath)
     Seq(
       ".*/(?!bugspots)[^/]*" -> s"""
-        |$$('ul.headmenu').map(function(i) {
+        |$$('div.main-sidebar ul:not(ul#system-admin-menu-container)').map(function(i) {
         |  var owner = $$("input[type=hidden][name=owner]").val();
         |  var repository = $$("input[type=hidden][name=repository]").val();
-        |  var s = $$(this);
-        |  var lc = s.children(':last').remove().clone();
-        |  s.append(
-        |    $$('<li></li>').append(
-        |      $$('<a><i class="menu-icon octicon octicon-bug"></i> Bug Spots</a>').attr('href', '${path}/' + owner + '/' + repository + '/bugspots')
-        |    )
-        |  );
-        |  s.append(lc);
+        |  if (owner && repository) {
+        |    var s = $$(this);
+        |    var lc = s.children(':last').remove().clone();
+        |    s.append(
+        |      $$('<li></li>').append(
+        |        $$('<a><i class="menu-icon octicon octicon-bug"></i> Bug Spots</a>').attr('href', '${path}/' + owner + '/' + repository + '/bugspots')
+        |      )
+        |    );
+        |    s.append(lc);
+        |  }
         |});
       """.stripMargin,
       ".*/bugspots" -> s"""
-        |$$('ul.headmenu').map(function(i) {
+        |$$('div.main-sidebar ul:not(ul#system-admin-menu-container)').map(function(i) {
         |  var owner = $$("input[type=hidden][name=owner]").val();
         |  var repository = $$("input[type=hidden][name=repository]").val();
-        |  var s = $$(this);
-        |  var lc = s.children(':last').remove().clone();
-        |  s.append(
-        |    $$('<li class="active"></li>').append(
-        |      $$('<a><i class="menu-icon menu-icon-active octicon octicon-bug"></i> Bug Spots</a>').attr('href', '${path}/' + owner + '/' + repository + '/bugspots')
-        |    )
-        |  );
-        |  s.append(lc);
+        |  if (owner && repository) {
+        |    var s = $$(this);
+        |    var lc = s.children(':last').remove().clone();
+        |    s.append(
+        |       $$('<li class="active"></li>').append(
+        |         $$('<a><i class="menu-icon menu-icon-active octicon octicon-bug"></i> Bug Spots</a>').attr('href', '${path}/' + owner + '/' + repository + '/bugspots')
+        |      )
+        |    );
+        |    s.append(lc);
+        |  }
         |});
       """.stripMargin)
   }
