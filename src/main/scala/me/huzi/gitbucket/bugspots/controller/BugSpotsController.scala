9b6d04b740c330038096a019f87a10542f825858
Ikebe Shodai <SIkebe@users.noreply.github.com>
2019-08-15 22:58:12 +0900

    Bump to GitBucket 4.32.0
    * GitBucket 4.32.0
    * scala 2.13.0
    * Use sbt-gitbucket-plugin
    * sbt 1.2.8

--- src/main/scala/me/huzi/gitbucket/bugspots/controller/BugSpotsController.scala
+++ src/main/scala/me/huzi/gitbucket/bugspots/controller/BugSpotsController.scala
@@ -9,6 +9,7 @@ import gitbucket.core.util.SyntaxSugars._
 import gitbucket.core.util.Directory._
 import me.huzi.gitbucket.bugspots.html
 import me.huzi.gitbucket.bugspots.util._
+import scala.util.Using
 
 class BugSpotsController extends BugSpotsControllerBase
   with RepositoryService with AccountService
@@ -21,12 +22,12 @@ trait BugSpotsControllerBase extends ControllerBase {
   private val DEFAULT_REGEX = """(?i).*\b(fix(ed|es)?|close(s|d)?)\b.*""".r
 
   get("/:owner/:repository/bugspots")(referrersOnly { repository =>
-    using(Git.open(getRepositoryDir(repository.owner, repository.name))) { git =>
+    Using.resource(Git.open(getRepositoryDir(repository.owner, repository.name))) { git =>
       if (JGitUtil.isEmpty(git)) {
         html.guide(repository)
       } else {
         val regex = params.get("regex").map(_.r).getOrElse(DEFAULT_REGEX)
-        using(new RevWalk(git.getRepository)) { revWalk =>
+        Using.resource(new RevWalk(git.getRepository)) { revWalk =>
           val fixes = BugSpotUtil.getFixList(git, revWalk, repository.repository.defaultBranch) { rc =>
             regex.findFirstIn(rc.getFullMessage).nonEmpty
           }
